20e3b99ef8b491f88791560af7459dee0f4a1b32
Pedro <pedro3francozo@gmail.com>
2026-01-11 10:16:15 -0300

    Study: Preliminary notes - Introduction to the dictionary

--- 76-introducao-dict/76_introducao_dicionario.py
+++ 76-introducao-dict/76_introducao_dicionario.py
@@ -3,16 +3,6 @@ Tópico: Introdução a Dicionários em python
 foco: Dicionário são estrutura de dados do tipo par de "chave" e "valor"
 """
 
-# Chaves podem ser consideradas como o "índice"
-# que vimos na lista e podem ser de tipos imutáveis
-# como: str, int, float, bool, tuple, etc.
-# O valor pode ser de qualquer tipo, incluindo outro
-# dicionário.
-# Usamos as chaves - {} - ou a classe dict para criar
-# dicionários.
-# Imutáveis: str, int, float, bool, tuple
-# Mutável: dict, list
-
 # pessoa = {
 #     'nome': 'Luiz Otávio',
 #     'sobrenome': 'Miranda',
