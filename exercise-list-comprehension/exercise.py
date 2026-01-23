b108344cec1bfcd7dd2737f237f86d171e917a4e
Pedro <pedro3francozo@gmail.com>
2026-01-23 20:29:23 -0300

    exercise

--- exercise-list-comprehension/exercise.py
+++ exercise-list-comprehension/exercise.py
@@ -3,10 +3,7 @@ Topic:
 """
 
 # Tabuada de 5 Nível Básico de mapeamento
-tabuada = [
-    n * 5
-    for n in range(1, 11) 
-]
+tabuada = [ n * 5 for n in range(1, 11) ]
 
 print(tabuada)
 
@@ -14,18 +11,13 @@ print(tabuada)
 
 numeros = [1, 2, 3, 4, 5]
 
-quadrados = [
-    n ** 2
-    for n in numeros
-]
+quadrados = [ n ** 2 for n in numeros ]
 
 print(quadrados)
 
 # outra maneira de resolver o execício ao quadrado
 
-number = [
-    n ** 2 for n in range(1, 6)
-]
+number = [ n ** 2 for n in range(1, 6) ]
 
 print(number)
 
@@ -33,28 +25,19 @@ print(number)
 
 nomes = ['luiz', 'maria', 'aline']
 
-gritos = [
-    nome.upper()
-    for nome in nomes
-]
+gritos = [ nome.upper() for nome in nomes ]
 
 print(gritos)
 
 # Apenas pares Nível 2
 
-pares = [    
-    n for n in range(21)
-    if n % 2 == 0
-]
+pares = [n for n in range(21) if n % 2 == 0 ]
 
 print(pares)
 
 # Nomes curtos 
 
-curtos = [
-    nome for nome in nomes
-    if len(nome) < 5
-]
+curtos = [ nome for nome in nomes if len(nome) < 5 ]
 
 print(curtos)
 
@@ -62,15 +45,9 @@ print(curtos)
 
 precos = [1.50, 4.99, 10.00, 85.20, 3.40]
 
-baratos = [
-    p for p in precos
-    if p < 5
-]
+baratos = [ p for p in precos if p < 5 ]
 
-promocao = [
-    p * 0.9
-    if p >= 10 else p for p in precos  
-]
+promocao = [ p * 0.9 if p >= 10 else p for p in precos  ]
 
 print(baratos)
 print(promocao)
