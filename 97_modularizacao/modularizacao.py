01c5ba1c465cbf29256b724615e5695c39eef40c
Pedro <pedro3francozo@gmail.com>
2026-01-28 19:41:26 -0300

    Study: Preliminary Notes - Modularização

---
+++ 97_modularizacao/modularizacao.py
@@ -0,0 +1,5 @@
+import modularizacao_ex
+
+print('Este módulo se chama', __name__)
+print(modularizacao_ex.variavel_modulo)
+print(modularizacao_ex.soma(2, 3))
---
+++ 97_modularizacao/modularizacao_ex.py
@@ -0,0 +1,6 @@
+print('Este módulo se chama', __name__)
+variavel_modulo = 'Pedro'
+
+
+def soma(x, y):
+    return x + y
