5ca74f230d5bc4ab750ebec8c89e0cde02df94b0
Pedro <pedro3francozo@gmail.com>
2026-01-28 22:37:17 -0300

    Study: Preliminary Notes - Introdução Packages

---
+++ 99_packages/99_utilizando_init.py
@@ -0,0 +1,6 @@
+import exemplo_99_packages
+
+print(exemplo_99_packages.dobra(2))
+print(exemplo_99_packages.soma_do_modulo(2, 3))
+
+# Utilizar o arquivo fora da pasta
---
+++ exemplo_99_packages/__init__.py
@@ -0,0 +1,8 @@
+from exemplo_99_packages.packages import variavel, soma_do_modulo
+
+print(
+    'Você importou', __name__
+)
+
+def dobra(x):
+    return x * 2
