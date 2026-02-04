e2d9b40057b9df7c393ce2752c87515725565870
Pedro <pedro3francozo@gmail.com>
2026-02-04 20:28:19 -0300

    exercise

--- 100_exercicio/exercicio.py
+++ 100_exercicio/exercicio.py
@@ -23,6 +23,13 @@ novos_produtos = [
     for p in copy.deepcopy(produtos)
 ]
 
+produtos_ordenados_por_nome = sorted(
+    copy.deepcopy(produtos),
+    key=lambda p: p['nome']
+)
+
 print(*produtos, sep='\n')
 print()
 print(*novos_produtos, sep='\n')
+print()
+print(*produtos_ordenados_por_nome, sep='\n')
